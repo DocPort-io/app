da8495169568f3ee9824a4183bf90559ed775321
Jonas Claes <jonas@jonasclaes.be>
2025-02-16 19:55:31 +0100

    feat: enhance message input with alert component and terminal icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
-    import { Input } from "$lib/components/ui/input";
-	import { Label } from "$lib/components/ui/label";
+	import Terminal from 'lucide-svelte/icons/terminal';
+	import * as Alert from '$lib/components/ui/alert';
+	import { Input } from '$lib/components/ui/input';
+	import { Label } from '$lib/components/ui/label';
 
 	let message = $state('');
 </script>
 
-<!-- <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
-
 <Label for="message">Message</Label>
-<Input type="text" placeholder="Enter some text" class="max-w-xs" id="message" />
+<Input type="text" placeholder="Enter some text" class="max-w-xs" id="message" bind:value={message} />
 
-<sl-alert open>
-	<sl-icon slot="icon" name="info-circle"></sl-icon>
-	{message}
-</sl-alert>
-
-<sl-qr-code label="Scan this code!" value={message}></sl-qr-code>
+<Alert.Root>
+	<Terminal class="h-4 w-4" />
+	<Alert.Title>Heads up!</Alert.Title>
+	<Alert.Description>
+		This is a warning message. Please be careful.
+		{message}
+	</Alert.Description>
+</Alert.Root>
