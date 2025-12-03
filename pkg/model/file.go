d3b8842bdd06a09b53275cb19572ad5c3049b058
Jonas Claes <jonas@jonasclaes.be>
2025-12-03 22:20:24 +0100

    refactor: remove unused GORM models
    - Deleted unused models: `BaseModel`, `File`, `Location`, `Project`, `User`, and `Version`.
    - Cleaned up associated dependencies and redundant files in the `model` package.

--- pkg/model/file.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type File struct {
-	BaseModel
-	Name     string
-	Size     int64
-	Path     string
-	Versions []Version `gorm:"many2many:version_files;"`
-}
--- pkg/model/version.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Version struct {
-	BaseModel
-	Name        string
-	Description string
-	ProjectId   uint
-	Files       []File `gorm:"many2many:version_files;"`
-}
