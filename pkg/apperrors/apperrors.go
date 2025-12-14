175b30ad4bf8e2b46e00a2fbf9a34966960baca8
Jonas Claes <jonas@jonasclaes.be>
2025-12-14 13:45:07 +0100

    refactor: remove `StatusText` field from `ErrResponse` struct
    - Eliminated the `StatusText` field as it was redundant.
    - Updated error response constructors to reflect the removal.

--- pkg/apperrors/apperrors.go
+++ pkg/apperrors/apperrors.go
@@ -10,9 +10,8 @@ type ErrResponse struct {
 	Err            error `json:"-"`
 	HTTPStatusCode int   `json:"-"`
 
-	StatusText string `json:"status"`
-	AppCode    int64  `json:"code,omitempty"`
-	ErrorText  string `json:"error,omitempty"`
+	AppCode   int64  `json:"code,omitempty"`
+	ErrorText string `json:"error,omitempty"`
 }
 
 func (e *ErrResponse) Render(w http.ResponseWriter, r *http.Request) error {
@@ -24,7 +23,6 @@ func ErrBadRequestError(err error) render.Renderer {
 	return &ErrResponse{
 		Err:            err,
 		HTTPStatusCode: 400,
-		StatusText:     "Bad Request",
 		ErrorText:      err.Error(),
 	}
 }
@@ -33,7 +31,6 @@ func ErrInternalServerError(err error) render.Renderer {
 	return &ErrResponse{
 		Err:            err,
 		HTTPStatusCode: 500,
-		StatusText:     "Internal Server Error",
 		ErrorText:      err.Error(),
 	}
 }
