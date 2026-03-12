61f73f86159776d436d58f16eb35c4cabc634fcf
Jonas Claes <jonas@jonasclaes.be>
2026-03-12 22:55:47 +0100

    chore(api): add OpenAPI spec and oapi-codegen configuration
    - Introduced `openapi-spec.yaml` documenting API endpoints, schemas, parameters, and responses.
    - Added `oapi-codegen.yml` for generating API models and client code.
    - Updated `generate.go` to include `oapi-codegen` command.

--- generate.go
+++ generate.go
@@ -1,3 +1,4 @@
+//go:generate oapi-codegen -config oapi-codegen.yml ./openapi-spec.yaml
 //go:generate sqlc generate -f sqlc.yaml
 //go:generate swag init -g ./pkg/app/server.go -o ./pkg/docs --parseDependency --parseInternal --useStructName
 
