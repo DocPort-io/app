7a2e4d3e9cdaab8ee26ae2a33d77638b0f72dcd6
Jonas Claes <jonas@jonasclaes.be>
2026-03-13 20:42:03 +0100

    chore(deps): update `go.sum` with new and updated dependencies

--- generate.go
+++ generate.go
@@ -1,5 +1,5 @@
-//go:generate oapi-codegen -config oapi-codegen.yml ./openapi-spec.yaml
-//go:generate sqlc generate -f sqlc.yaml
-//go:generate swag init -g ./pkg/app/server.go -o ./pkg/docs --parseDependency --parseInternal --useStructName
+//go:generate go tool oapi-codegen -config oapi-codegen.yml ./openapi-spec.yaml
+//go:generate go tool sqlc generate -f sqlc.yaml
+//go:generate go tool swag init -g ./pkg/app/server.go -o ./pkg/docs --parseDependency --parseInternal --useStructName
 
 package app
